2c3f97ba48f0d32085417ed0b82002f9efc6a177
MaulyaSoni <24maulya@gmail.com>
2026-09-18 15:15:00 +0530

    improved strategy code

--- Week_5.1/Day_4/main.py
+++ Week_5.1/Day_4/main.py
@@ -3,24 +3,28 @@ from strategies import RegularCustomer ,MemberCustomer , VIPCustomer
 from context import Cart
 from functions import switch_customer_type , get_bill
 def main():
-    print("\n-------Classes Way :------")
-    cart_obj = Cart(RegularCustomer(1,1000))
-    print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
+    val = int(input("Enter 0 for Classical Way and 1 for Pythonic way :"))
+    if val == 0:
+        print("\n-------Classes Way (Classical) :------")
+        cart_obj = Cart(RegularCustomer(1,1000))
+        print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
 
-    cart_obj.set_customer_type(MemberCustomer(1,1000))
-    print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
+        cart_obj.set_customer_type(MemberCustomer(1,1000))
+        print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
 
-    cart_obj.set_customer_type(VIPCustomer(1,1000))
-    print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
-    
+        cart_obj.set_customer_type(VIPCustomer(1,1000))
+        print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
+        
+    elif val == 1:
+        print("\n-------Pythonic Way-------")
+        customer = switch_customer_type("regular")
+        print(get_bill(3 , 1000 , customer))
+        customer = switch_customer_type("member")
+        print(get_bill(3 , 1000 , customer))
+        customer = switch_customer_type("vip")
+        print(get_bill(3 , 1000 , customer))
 
-    print("\n-------Pythonic Way-------")
-    customer = switch_customer_type("regular")
-    print(get_bill(3 , 1000 , customer))
-    customer = switch_customer_type("member")
-    print(get_bill(3 , 1000 , customer))
-    customer = switch_customer_type("vip")
-    print(get_bill(3 , 1000 , customer))
-
+    else:
+        print("Invalid input")
 if __name__ == "__main__":
     main()
