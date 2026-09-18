b736b76bc5331f8089bbeeadaf72a6c226e9b6f9
MaulyaSoni <24maulya@gmail.com>
2026-09-18 15:11:59 +0530

    Strategy done with Pythonic way

---
+++ Week_5.1/Day_4/functions.py
@@ -0,0 +1,38 @@
+def calculate_discount(total , discount):
+    discount = discount
+    disc_temp = discount / 100
+    disc_temp *= total
+    discounted_total = total - disc_temp
+    return discounted_total
+
+def regular_customer(items , total):
+    result  = calculate_discount(total , 5)
+    return ("Regular Customer : ",result)
+
+def member_customer(items , total):
+    result  = calculate_discount(total , 10)
+    return ("Member Customer : ",result)
+
+def vip_customer(items , total):
+    result  = calculate_discount(total , 15)
+    return ("VIP Customer : ",result)
+
+def switch_customer_type(val : str) ->str :
+    if val.lower() == "regular":
+        customer = "regular"
+    elif val.lower() == "member":
+        customer = "member"
+    elif val.lower() == "vip":
+        customer = "vip"
+    return customer
+
+def get_bill(items , total , customer):
+    if customer == "regular":
+        result = regular_customer(items , total)
+    elif customer == "member":
+        result = member_customer(items , total)    
+    elif customer == "vip":
+        result = vip_customer(items , total)
+    
+    return (f"{customer} :- {result}")
+
--- Week_5.1/Day_4/main.py
+++ Week_5.1/Day_4/main.py
@@ -1,17 +1,26 @@
 from interface import Bill
 from strategies import RegularCustomer ,MemberCustomer , VIPCustomer
 from context import Cart
-
+from functions import switch_customer_type , get_bill
 def main():
+    print("\n-------Classes Way :------")
     cart_obj = Cart(RegularCustomer(1,1000))
     print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
 
     cart_obj.set_customer_type(MemberCustomer(1,1000))
-    # vip = VIPCustomer(1,1000)
     print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
 
     cart_obj.set_customer_type(VIPCustomer(1,1000))
     print(f"Customer : {cart_obj.show_customer_type()} , Bill :{cart_obj.display_bill()}")
     
+
+    print("\n-------Pythonic Way-------")
+    customer = switch_customer_type("regular")
+    print(get_bill(3 , 1000 , customer))
+    customer = switch_customer_type("member")
+    print(get_bill(3 , 1000 , customer))
+    customer = switch_customer_type("vip")
+    print(get_bill(3 , 1000 , customer))
+
 if __name__ == "__main__":
     main()
